8a03b4f2e27d2bbb76bcfa8a45fd6758893deee6
Unver Akmandor <akmandor.n@northeastern.edu>
2022-01-28 14:24:08 -0500

    Updated util/util.py: added make_vec_env_openairos function to support custom env by openai_ros architecture.

--- src/imitation/util/util.py
+++ src/imitation/util/util.py
@@ -1,4 +1,20 @@
-"""Miscellaneous utility methods."""
+#!/usr/bin/env python3
+
+'''
+LAST UPDATE: 2022.01.28
+
+AUTHOR: HumanCompatibleAI/imitation
+        Neset Unver Akmandor (NUA)
+
+E-MAIL: akmandor.n@northeastern.edu
+
+DESCRIPTION: TODO..."""Miscellaneous utility methods."""
+
+REFERENCES:
+
+NUA TODO:
+- 
+'''
 
 import datetime
 import functools
@@ -21,9 +37,12 @@ import gym
 import numpy as np
 import torch as th
 from gym.wrappers import TimeLimit
+
 from stable_baselines3.common import monitor
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv, VecEnv
+from stable_baselines3.common.monitor import Monitor
 
+from openai_ros.openai_ros_common import StartOpenAI_ROS_Environment
 
 def make_unique_timestamp() -> str:
     """Timestamp, with random uuid added to avoid collisions."""
@@ -69,6 +88,8 @@ def make_vec_env(
     # Resolve the spec outside of the subprocess first, so that it is available to
     # subprocesses running `make_env` via automatic pickling.
     spec = gym.spec(env_name)
+    #print("imitation::util::make_vec_env -> spec type: " + str(type(spec)))
+
     env_make_kwargs = env_make_kwargs or {}
 
     def make_env(i, this_seed):
@@ -81,16 +102,19 @@ def make_vec_env(
         # work. For more discussion and hypotheses on this issue see PR #160:
         # https://github.com/HumanCompatibleAI/imitation/pull/160.
         env = spec.make(**env_make_kwargs)
+        #print("imitation::util::make_vec_env::make_env -> env type: " + str(type(env)))
 
         # Seed each environment with a different, non-sequential seed for diversity
         # (even if caller is passing us sequentially-assigned base seeds). int() is
         # necessary to work around gym bug where it chokes on numpy int64s.
         env.seed(int(this_seed))
+        #print("imitation::util::make_vec_env::make_env -> env type: " + str(type(env)))
 
         if max_episode_steps is not None:
             env = TimeLimit(env, max_episode_steps)
         elif spec.max_episode_steps is not None:
             env = TimeLimit(env, max_episode_steps=spec.max_episode_steps)
+        #print("imitation::util::make_vec_env::make_env -> env type: " + str(type(env)))
 
         # Use Monitor to record statistics needed for Baselines algorithms logging
         # Optionally, save to disk
@@ -101,6 +125,7 @@ def make_vec_env(
             log_path = os.path.join(log_subdir, f"mon{i:03d}")
 
         env = monitor.Monitor(env, log_path)
+        #print("imitation::util::make_vec_env::make_env -> env type: " + str(type(env)))
 
         if post_wrappers:
             for wrapper in post_wrappers:
@@ -110,13 +135,124 @@ def make_vec_env(
 
     rng = np.random.RandomState(seed)
     env_seeds = rng.randint(0, (1 << 31) - 1, (n_envs,))
+    #print("imitation::util::make_vec_env -> env_seeds type: " + str(type(env_seeds)))
+    #print("imitation::util::make_vec_env -> env_seeds shape: " + str(env_seeds.shape))
+    #print("imitation::util::make_vec_env -> env_seeds: " + str(env_seeds))
+
     env_fns = [functools.partial(make_env, i, s) for i, s in enumerate(env_seeds)]
+    #print("imitation::util::make_vec_env -> env_fns type: " + str(type(env_fns)))
+    #print("imitation::util::make_vec_env -> env_fns len: " + str(len(env_fns)))
+    #print("imitation::util::make_vec_env -> env_fns 0 type: " + str(type(env_fns[0])))
+
+
     if parallel:
         # See GH hill-a/stable-baselines issue #217
         return SubprocVecEnv(env_fns, start_method="forkserver")
     else:
         return DummyVecEnv(env_fns)
 
+def make_vec_env_openairos(
+    env_name: str,
+    n_envs: int = 1,
+    seed: int = 0,
+    parallel: bool = False,
+    log_dir: Optional[str] = None,
+    max_episode_steps: Optional[int] = None,
+    post_wrappers: Optional[Sequence[Callable[[gym.Env, int], gym.Env]]] = None,
+    env_make_kwargs: Optional[Mapping[str, Any]] = None,
+) -> VecEnv:
+    """Makes a vectorized environment.
+
+    Args:
+        env_name: The Env's string id in Gym.
+        n_envs: The number of duplicate environments.
+        seed: The environment seed.
+        parallel: If True, uses SubprocVecEnv; otherwise, DummyVecEnv.
+        log_dir: If specified, saves Monitor output to this directory.
+        max_episode_steps: If specified, wraps each env in a TimeLimit wrapper
+            with this episode length. If not specified and `max_episode_steps`
+            exists for this `env_name` in the Gym registry, uses the registry
+            `max_episode_steps` for every TimeLimit wrapper (this automatic
+            wrapper is the default behavior when calling `gym.make`). Otherwise
+            the environments are passed into the VecEnv unwrapped.
+        post_wrappers: If specified, iteratively wraps each environment with each
+            of the wrappers specified in the sequence. The argument should be a Callable
+            accepting two arguments, the Env to be wrapped and the environment index,
+            and returning the wrapped Env.
+        env_make_kwargs: The kwargs passed to `spec.make`.
+
+    Returns:
+        A VecEnv initialized with `n_envs` environments.
+    """
+    # Resolve the spec outside of the subprocess first, so that it is available to
+    # subprocesses running `make_env` via automatic pickling.
+    #spec = gym.spec(env_name)
+    env_make_kwargs = env_make_kwargs or {}
+
+    def make_env(i, this_seed):
+        # Previously, we directly called `gym.make(env_name)`, but running
+        # `imitation.scripts.train_adversarial` within `imitation.scripts.parallel`
+        # created a weird interaction between Gym and Ray -- `gym.make` would fail
+        # inside this function for any of our custom environment unless those
+        # environments were also `gym.register()`ed inside `make_env`. Even
+        # registering the custom environment in the scope of `make_vec_env` didn't
+        # work. For more discussion and hypotheses on this issue see PR #160:
+        # https://github.com/HumanCompatibleAI/imitation/pull/160.
+        #env = spec.make(**env_make_kwargs)
+
+        # Init OpenAI_ROS ENV
+        env = StartOpenAI_ROS_Environment(env_name, robot_id=0, max_episode_steps=max_episode_steps, data_folder_path=log_dir)
+        #print("imitation::util::make_vec_env_openairos::make_env -> env type: " + str(type(env)))
+
+        # Seed each environment with a different, non-sequential seed for diversity
+        # (even if caller is passing us sequentially-assigned base seeds). int() is
+        # necessary to work around gym bug where it chokes on numpy int64s.
+        env.seed(int(this_seed))
+
+        '''
+        if max_episode_steps is not None:
+            env = TimeLimit(env, max_episode_steps)
+        elif spec.max_episode_steps is not None:
+            env = TimeLimit(env, max_episode_steps=spec.max_episode_steps)
+        '''
+
+        '''
+        # Use Monitor to record statistics needed for Baselines algorithms logging
+        # Optionally, save to disk
+        log_path = None
+        if log_dir is not None:
+            log_subdir = os.path.join(log_dir, "monitor")
+            os.makedirs(log_subdir, exist_ok=True)
+            log_path = os.path.join(log_subdir, f"mon{i:03d}")
+
+        env = monitor.Monitor(env, log_path)
+        '''
+
+        env = Monitor(env, log_dir)
+        #print("imitation::util::make_vec_env_openairos::make_env -> env type: " + str(type(env)))
+
+        if post_wrappers:
+            for wrapper in post_wrappers:
+                env = wrapper(env, i)
+
+        return env
+
+    rng = np.random.RandomState(seed)
+    env_seeds = rng.randint(0, (1 << 31) - 1, (n_envs,))
+    #print("imitation::util::make_vec_env_openairos -> env_seeds type: " + str(type(env_seeds)))
+    #print("imitation::util::make_vec_env_openairos -> env_seeds shape: " + str(env_seeds.shape))
+    #print("imitation::util::make_vec_env_openairos -> env_seeds: " + str(env_seeds))
+
+    env_fns = [functools.partial(make_env, i, s) for i, s in enumerate(env_seeds)]
+    #print("imitation::util::make_vec_env_openairos -> env_fns type: " + str(type(env_fns)))
+    #print("imitation::util::make_vec_env_openairos -> env_fns len: " + str(len(env_fns)))
+    #print("imitation::util::make_vec_env_openairos -> env_fns 0 type: " + str(type(env_fns[0])))
+
+    if parallel:
+        # See GH hill-a/stable-baselines issue #217
+        return SubprocVecEnv(env_fns, start_method="forkserver")
+    else:
+        return DummyVecEnv(env_fns)
 
 def docstring_parameter(*args, **kwargs):
     """Treats the docstring as a format string, substituting in the arguments."""
